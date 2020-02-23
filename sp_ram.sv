81d70fcbb593ea5aa11ed83e60bc1f0e919f0a20
Stephano Cetola <scetola@linuxfoundation.org>
2020-02-23 12:15:07 -0800

    add a valid bit to the ram for read
    Signed-off-by: Stephano Cetola <scetola@linuxfoundation.org>

--- sp_ram.sv
+++ sp_ram.sv
@@ -14,21 +14,27 @@ module sp_ram
         output                  rvalid_o,
         input                   req_i,
         input                   we_i,
-        input                   be_i  //ignore byte enable for now
+        input   [3:0]           be_i  //ignore byte enable for now
     );
 
     reg [DATA_WIDTH-1:0]   tmp_data;
     reg [DATA_WIDTH-1:0]   mem [DEPTH];
+    reg rvalid;
 
     always @ (posedge clk_i) begin
-        if (we_i)
+        if (we_i) begin
             mem[addr_i] <= wdata_i;
+            rvalid <= 0;
+        end
     end
 
     always @ (posedge clk_i) begin
-        if (!we_i)
+        if (!we_i) begin
             tmp_data <= mem[addr_i];
+            rvalid <= 1;
+        end
     end
 
     assign rdata_o = req_i & !we_i ? tmp_data : 'hz;
+    assign rvalid_o = req_i & !we_i ? 1 : 0;
 endmodule
