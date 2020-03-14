c564d7e6dbcf8fe62e18770c40b06ab88401d631
Stephano Cetola <scetola@linuxfoundation.org>
2020-03-14 12:23:51 -0700

    move test code out of the BFM
    Now that my head is screwed on correctly and I've had enough coffee, it
    is pretty obvious that there is far too much testing code in the BFM and
    not nearly enough in the tester.
    
    I hope this works.
    
    Signed-off-by: Stephano Cetola <scetola@linuxfoundation.org>

--- tb/tester.svh
+++ tb/tester.svh
@@ -8,9 +8,41 @@ calling "init_mem" it is allowing random data and opcodes to be tested.
 import ibex_pkg::*;
 class tester;
 
+    /*
+      Opcode Generation Functions
+
+      The following functions connect to the Opcode Generator. This generator
+      abstracts out the idea of generating machine code so that the verilog
+      can only concern itself with functionality, not ISA implmentation.
+    */
+    import "DPI-C" function void make_test(input int op, output bit[(64*32-1):0] ram_buf, input int ram_words);
+    import "DPI-C" function void initGen();
+    import "DPI-C" function void setReg(int r1, int r2, int rd);
+    import "DPI-C" function void setArith(int arith1, int arith2);
+
+    typedef enum int
+    {
+        ARITH_ADD = 32'h0, //R-Type opcode field funct7 and funct3 shown on page 19
+        ARITH_SUB = 32'h40000000,
+        ARITH_SLL = 32'h00001000,
+        ARITH_XOR = 32'h00004000,
+        ARITH_SRL = 32'h00005000,
+        ARITH_OR = 32'h00006000,
+        ARITH_AND = 32'h00007000
+    } arithmetic_op_t;
+
+    // Let's not shift 32 bit values more than 31 units as that is unrealistic
+    class shft_t;
+        rand integer val;
+        constraint shft_range { val inside { [0:31]}; }
+    endclass
+    
+    int errors;
     virtual vip_bfm bfm;
 
     function new (virtual vip_bfm b);
+        shft_t shft = new();
+        errors = 0;
         bfm = b;
     endfunction : new
     
@@ -24,8 +56,75 @@ class tester;
                 @(negedge bfm.clk_sys);
             end
         end
+        $display("TESTER ERR TOTAL: %d", errors);
         bfm.end_sim();
     endtask : execute
+
+        // Load a random test into RAM
+    function load_test();
+        automatic bit [63:0][31:0] ram_buf;
+        bfm.currAluOp = getRandOp();
+        setArith(getRandData(), getRandData());
+        $display("===============Testing %s with %h and %h=================",
+            currAluOp.name, testArith1, testArith2);
+        case(currAluOp)
+            ALU_ADD: make_test(ARITH_ADD, ram_buf, 64);
+            ALU_SUB: make_test(ARITH_SUB, ram_buf, 64);
+            ALU_XOR: make_test(ARITH_XOR, ram_buf, 64);
+            ALU_OR: make_test(ARITH_OR, ram_buf, 64);
+            ALU_AND: make_test(ARITH_AND, ram_buf, 64);
+            ALU_SRL: begin
+                setArith(getRandData(), getRandShift()); 
+                make_test(ARITH_SRL, ram_buf, 64);
+            end
+            ALU_SLL: begin
+                setArith(getRandData(), getRandShift());
+                make_test(ARITH_SLL, ram_buf, 64);
+            end
+            default: throwError($sformatf("Unknown ALU Op: %s",bfm.currAluOp.name));
+        endcase
+        bfm.array_to_ram(ram_buf);
+    endfunction
+
+    /*
+        Randomization functions
+        Both opcodes and data are randomized.
+        We should also randomize the registers to ensure they all work.
+    */
+    function int getRandData();
+        bit [1:0] zero_ones;
+        zero_ones = $random;
+        if(zero_ones === 2'b00)
+            return 32'h00;
+        else if (zero_ones === 2'b11)
+            return 32'hff;
+        else
+            return $random;
+    endfunction
+
+    function int getRandShift();
+        shft.randomize();
+        return shft.val;
+    endfunction
+
+    function alu_op_e getRandOp();
+        bit [2:0] op_choice;
+        op_choice = $random;
+        casez(op_choice)
+            3'b000 : return ALU_ADD;
+            3'b001 : return ALU_SUB;
+            3'b010 : return ALU_XOR;
+            3'b011 : return ALU_OR;
+            3'b100 : return ALU_AND;
+            3'b101 : return ALU_SRL;
+            3'b110 : return ALU_SLL;
+        endcase
+    endfunction
+
+    task throwError(input string msg);
+        errors = errors +1;
+        $display("TESTER ERR: %s", msg);
+    endtask
 
 endclass : tester
 
--- vip_bfm.sv
+++ vip_bfm.sv
@@ -5,7 +5,6 @@ and provides a way of observing the DUT.
 
 `timescale 1us / 1ns
 import ibex_pkg::*;
-
 interface vip_bfm;
     
     //default to 1MHz
@@ -16,39 +15,6 @@ interface vip_bfm;
     parameter int          MEM_SIZE  = 64 * 1024; // 64 kB
     parameter logic [31:0] MEM_START = 32'h00000000;
     parameter logic [31:0] MEM_MASK  = MEM_SIZE-1;
-    
-    import "DPI-C" function void make_loadstore_test(output bit[(64*32-1):0] ram_buf, input int ram_words);
-    import "DPI-C" function void make_test(input int op, output bit[(64*32-1):0] ram_buf, input int ram_words);
-    import "DPI-C" function void initGen();
-    import "DPI-C" function void setReg(int r1, int r2, int rd);
-    import "DPI-C" function void setArith(int arith1, int arith2);
-
-    typedef enum int
-    {
-        ARITH_ADD = 32'h0, //R-Type opcode field funct7 and funct3 shown on page 19
-        ARITH_SUB = 32'h40000000,
-        ARITH_SLL = 32'h00001000,
-        ARITH_XOR = 32'h00004000,
-        ARITH_SRL = 32'h00005000,
-        ARITH_OR = 32'h00006000,
-        ARITH_AND = 32'h00007000
-    } arithmetic_op_t;
-
-    // These values are hard coded in opcode_generator.cpp as defaults
-    int testReg1 = 5;
-    int testReg2 = 6;
-    int testRegDest = 7;
-    int testArith1 = 1;
-    int testArith2 = 2;
-
-    // Let's not shift 32 bit values more than 31 units as that is unrealistic
-    class shft_t;
-        rand integer val;
-        constraint shft_range { val inside { [0:31]}; }
-    endclass
-    shft_t shft = new();
-
-    int errors = 0;
 
     alu_op_e currAluOp;
     opcode_e currOp;
@@ -115,41 +81,7 @@ interface vip_bfm;
             data_rvalid  <= ~instr_req && data_req && mem_req;
         end
     end
-
-    /*
-      Opcode Generation Functions
-
-      The following functions connect to the Opcode Generator. This generator
-      abstracts out the idea of generating machine code so that the verilog
-      can only concern itself with functionality, not ISA implmentation.
-    */
     
-    // Load a random test into RAM
-    function load_test();
-        automatic bit [63:0][31:0] ram_buf;
-        currAluOp = getRandOp();
-        setRandArith();
-        $display("===============Testing %s with %h and %h=================",
-            currAluOp.name, testArith1, testArith2);
-        case(currAluOp)
-            ALU_ADD: make_test(ARITH_ADD, ram_buf, 64);
-            ALU_SUB: make_test(ARITH_SUB, ram_buf, 64);
-            ALU_XOR: make_test(ARITH_XOR, ram_buf, 64);
-            ALU_OR: make_test(ARITH_OR, ram_buf, 64);
-            ALU_AND: make_test(ARITH_AND, ram_buf, 64);
-            ALU_SRL: begin
-                setRandArithShift(); 
-                make_test(ARITH_SRL, ram_buf, 64);
-            end
-            ALU_SLL: begin
-                setRandArithShift();
-                make_test(ARITH_SLL, ram_buf, 64);
-            end
-            default: throwError($sformatf("Unknown ALU Op: %s",bfm.currAluOp.name));
-        endcase
-        array_to_ram(ram_buf);
-    endfunction
-
     // Since the generator returns an array, use the verilator function to
     // "write" that into memory. Note that we're taking advantage of a
     // pre-made function of Ibex, which uses verilator:
@@ -161,75 +93,6 @@ interface vip_bfm;
         begin
             sp_ram.simutil_verilator_set_mem(i, ram_buf[i]);
         end
-    endfunction
-
-    // TODO: test load and store from memory regarless of ALU operation
-    function init_mem_loadstore();
-        automatic bit [63:0][31:0] ram_buf;
-        make_loadstore_test(ram_buf, 64);
-        array_to_ram(ram_buf);
-    endfunction
-
-    task throwError(input string msg);
-        errors = errors +1;
-        $display("BFM ERR: %s", msg);
-    endtask
-
-    // Setter Functions for Test Registers and Values
-    function setRegisters(input int reg1, input int reg2, input int regDest);
-        testReg1 = reg1;
-        testReg2 = reg2;
-        testRegDest = regDest;
-        setReg(reg1, reg2, regDest);
-    endfunction
-
-    function setRandArith();
-        setArithVals(getRandData(), getRandData());
-    endfunction
-
-    function setRandArithShift();
-        setArithVals(getRandData(), getRandShift());
-    endfunction
-
-    function setArithVals(input int arith1, input int arith2);
-        testArith1 = arith1;
-        testArith2 = arith2;
-        setArith(arith1, arith2);
-    endfunction
-
-    /*
-        Randomization functions
-        Both opcodes and data are randomized.
-        We should also randomize the registers to ensure they all work.
-    */
-    function int getRandData();
-        bit [1:0] zero_ones;
-        zero_ones = $random;
-        if(zero_ones === 2'b00)
-            return 32'h00;
-        else if (zero_ones === 2'b11)
-            return 32'hff;
-        else
-            return $random;
-    endfunction
-
-    function int getRandShift();
-        shft.randomize();
-        return shft.val;
-    endfunction
-
-    function alu_op_e getRandOp();
-        bit [2:0] op_choice;
-        op_choice = $random;
-        casez(op_choice)
-            3'b000 : return ALU_ADD;
-            3'b001 : return ALU_SUB;
-            3'b010 : return ALU_XOR;
-            3'b011 : return ALU_OR;
-            3'b100 : return ALU_AND;
-            3'b101 : return ALU_SRL;
-            3'b110 : return ALU_SLL;
-        endcase
     endfunction
 
     // Memory and Register Access Methods
@@ -249,7 +112,6 @@ interface vip_bfm;
     endtask
 
     task end_sim;
-        $display("BFM ERR TOTAL: %d", errors);
         $stop;
     endtask
     
